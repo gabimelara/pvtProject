771e09eae2dfc0ef291ab8bf2a2be77184ac22f5
Gabriela Melara <gabi14melara@hotmail.com>
2021-04-20 12:13:21 +0200

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,16 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication SpringBootServletInitializer{
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value ="name", defaultValue = "World")String name){
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
-}
-
-
+	}
 }
